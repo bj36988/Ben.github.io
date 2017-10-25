b870bf4c879fae06f25ac0814b3f21d1326a3aba
bj36988 <32454941+bj36988@users.noreply.github.com>
2017-10-24 21:45:24 -0500

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,41 @@
+// Image switcher code
+
+
+ var myImage = document.querySelector('img');
+
+
+ myImage.onclick = function() {
+ 	var mySrc = myImage.getAttribute('src');
+ 	if(mySrc === 'images/brain.jpg') {
+       myImage.setAttribute ('src','images/mountain.jpg');
+ 	} else {
+ 	  myImage.setAttribute ('src','images/brain.jpg');
+ 	}
+ }
+
+
+ // Personalized welcome message code
+
+
+ var myButton = document.querySelector('button');
+ var myHeading = document.querySelector('h1');
+
+
+ function setUserName() {
+   var myName = prompt('Please enter your name.');
+   localStorage.setItem('name', myName);
+   myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+ }
+
+
+ if(!localStorage.getItem('name')) {
+   setUserName();
+ } else {
+   var storedName = localStorage.getItem('name');
+   myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+ }
+
+
+ myButton.onclick = function() {
+   setUserName();
+ }
